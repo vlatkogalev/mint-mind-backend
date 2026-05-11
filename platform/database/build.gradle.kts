8749552b3f5aa84698f84a4fd4c5c6cc2e7a2bd3
Vlatko Galev <vlatko.galev89@gmail.com>
2026-05-11 13:24:58 +0200

    Fix 'org.flywaydb.core.api.FlywayException: Unsupported Database: PostgreSQL 15.17' error.

--- platform/database/build.gradle.kts
+++ platform/database/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(project(":platform:core"))
     implementation(ktorLibs.server.core)
     implementation(libs.flyway.core)
+    implementation(libs.flyway.postgresql)
     implementation(libs.hikari.cp)
     implementation(libs.h2database.h2)
     implementation(libs.postgresql)
