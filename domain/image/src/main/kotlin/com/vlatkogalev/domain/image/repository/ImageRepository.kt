f2cbc68ef3969e8d493b8b07152e702830311425
Vlatko Galev <vlatko.galev89@gmail.com>
2026-05-13 12:21:23 +0200

    Fix bug in ImageRepository where findByUserId accepted Long for userId instead of UUID.

--- domain/image/src/main/kotlin/com/vlatkogalev/domain/image/repository/ImageRepository.kt
+++ domain/image/src/main/kotlin/com/vlatkogalev/domain/image/repository/ImageRepository.kt
@@ -1,8 +1,9 @@
 package com.vlatkogalev.domain.image.repository
 
 import com.vlatkogalev.domain.image.model.Image
+import java.util.UUID
 
 interface ImageRepository {
     suspend fun save(image: Image): Image
-    suspend fun findByUserId(userId: Long): List<Image>
+    suspend fun findByUserId(userId: UUID): List<Image>
 }
